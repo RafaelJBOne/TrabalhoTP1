1186e844f5a2fe6cb5a6458efe462720ea622294
JohnatanSR <79852482+JohnatanSR@users.noreply.github.com>
2021-03-11 11:32:38 -0300

    Add files via upload

---
+++ tbpteste/TP1_Trab/mainTP1.cpp
@@ -0,0 +1,21 @@
+// Exemplos desenvolvidos para ilustrar a sintaxe da linguagem.
+#include <iostream>
+#include "dominiosTP1.h"
+//#include "entidades.h"
+#include "testesTP1.h"
+
+using namespace std;
+
+int main(){
+    TUNumero testeA;
+
+    switch(testeA.executar()){
+        case TUNumero::SUCESSO: cout << "SUCESSO - CODIGO" << endl;
+                                break;
+        case TUNumero::FALHA  : cout << "FALHA   - CODIGO" << endl;
+                                break;
+    }
+    //cout << testeA.FALHA; print do c++
+
+    return 0;
+}
